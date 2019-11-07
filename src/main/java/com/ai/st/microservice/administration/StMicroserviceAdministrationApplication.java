64abe15dd91d420a0df19687e210a154d6cf1ad5
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-11-07 13:12:02 -0500

    applying configuration from config server and support with actuator

--- src/main/java/com/ai/st/microservice/administration/StMicroserviceAdministrationApplication.java
+++ src/main/java/com/ai/st/microservice/administration/StMicroserviceAdministrationApplication.java
@@ -3,8 +3,10 @@ package com.ai.st.microservice.administration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EntityScan({"com.ai.st.entities"})
 public class StMicroserviceAdministrationApplication {
 
